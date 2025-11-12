e66a27cc3ec7892f12e1e204c686125e6e3b2541
YTY3212 <146659933+YTY3212@users.noreply.github.com>
2025-11-12 09:42:09 +0200

    Add copyright and license information to script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 YTY3212
+// Licensed under the MIT License.
+// See LICENSE file in the project root for details.
+
 function showTime() {
 	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
 }
